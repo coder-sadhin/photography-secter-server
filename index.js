cac1f4064ca28a7477af6bf8e46325fdcc3ac2fb
coder-sadhin <codersadhin@gmail.com>
2022-11-08 15:34:28 +0600

    specific servics data colection url

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -16,7 +17,6 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://photography_user:7xzReybfRLfc9kvz@cluster0.bbbtstv.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -45,9 +45,6 @@ const run = async () => {
             res.send(service);
         })
 
-
-
-
         app.get('/blogs', async (req, res) => {
             const query = {};
             const cursor = blogsCollection.find(query);
@@ -55,8 +52,12 @@ const run = async () => {
             res.send(service);
         })
 
-
-
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
 
 
 
